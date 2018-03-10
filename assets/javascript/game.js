cfb2ec57ae7c60b4d1df49df7913b4c9f123ca48
Josh Parker <jthomasparker@gmail.com>
2018-03-10 08:46:49 -0500

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
+
+
 window.onload = function() {
-    
+ // Click events for the crystals   
     $("#crys-blue").on("click", function(){
         updateScore(crysBlue)
     });
@@ -13,24 +15,24 @@ window.onload = function() {
         updateScore(crysYellow)
     });
 
+// Click events for the scoreboard/instructions buttons
     $("#scoreboard").on("click", function(){
         $("#scoreboard").css("filter", "brightness(80%)");
         $("#instructions").css("filter", "brightness(100%)");
-
     });
-
     $("#instructions").on("click", function(){
         $("#instructions").css("filter", "brightness(80%)");
         $("#scoreboard").css("filter", "brightness(100%)");
-
     });
 
+// sets initial instructions button brightness
     $("#instructions").css("filter", "brightness(80%)");
+
+// start new game
     newGame();
-   
 };
 
-
+// global variables
 var winCount = 0;
 var lossCount = 0;
 var crysBlue = "";
@@ -41,13 +43,15 @@ var goal = 0;
 var score = 0;
 
 
-
+// generates random number when given min/max values (min/max are inclusive)
 function generateRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
 
-
+// starts new game
 function newGame(){
+
+// update variables with new random numnbers (except score goes back to 0)
 goal = generateRandom(19, 120);
 score = 0;
 crysBlue = generateRandom(1, 12);
@@ -55,21 +59,22 @@ crysGreen = generateRandom(1, 12);
 crysPurple = generateRandom(1, 12);
 crysYellow = generateRandom(1, 12);
 
+// update corresponding elements with new variable values
 $("#goal").text(goal);
 $("#score").text(score);
 $("#wins").text("Wins: " + winCount);
 $("#losses").text("Losses: " + lossCount);
-
-
 };
 
+// updates score when passed which crystal was clicked
 function updateScore(crystalClicked){
     console.log(crystalClicked);
     score += crystalClicked;
     $("#score").text(score);
     gameStatus();
 }
-
+// checks to see if player has won/loss yet and 
+// starts a new game if either condition is true
 function gameStatus(){
     if(score > goal){
         lossCount++
